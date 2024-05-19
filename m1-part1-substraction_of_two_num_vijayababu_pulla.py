0065bc7681177760ee9f68b2eceacfc20d8d9e10
Vijayababu <pvijayababu1@gmail.com>
2024-05-18 21:40:02 -0400

    Substraction of two numbers

---
+++ m1-part1-substraction_of_two_num_vijayababu_pulla.py
@@ -0,0 +1,11 @@
+# Step 1: Prompt the user to enter the first number
+num1 = float(input("Enter the first number: "))
+
+# Step 2: Prompt the user to enter the second number
+num2 = float(input("Enter the second number: "))
+
+# Step 3: Add the two numbers and store the result
+substraction = num1 - num2
+
+# Step 4: Print the result
+print("The sum of", num1, "and", num2, "is", substraction)
